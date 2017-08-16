216e2983dda3ca17dba07f35ea9029a48addf273
youyun <youyun@andacx.com>
2017-08-16 16:55:38 +0800

    添加高德地图jar包和so文件

--- app/src/main/java/com/lib/amap/LibAmap.java
+++ app/src/main/java/com/lib/amap/LibAmap.java
@@ -6,7 +6,7 @@ import android.util.Log;
  * @Author moyuwan
  * @Date 17/8/16
  */
-public class LibAmap {
+public class LibAmap implements LibAmapInterface {
 
     public static void test(String content) {
         Log.e("LibAmap", content);
---
+++ app/src/main/java/com/lib/amap/LibAmapInterface.java
@@ -0,0 +1,13 @@
+package com.lib.amap;
+
+/**
+ * LibAmap对外提供的接口
+ *
+ * @Author moyuwan
+ * @Date 17/8/16
+ */
+public interface LibAmapInterface {
+
+
+
+}
