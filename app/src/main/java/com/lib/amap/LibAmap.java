4b44335d848a23fb2060544efe98b62313cdb94e
youyun <youyun@andacx.com>
2017-08-16 11:36:21 +0800

    Initial Commit

---
+++ app/src/main/java/com/lib/amap/LibAmap.java
@@ -0,0 +1,15 @@
+package com.lib.amap;
+
+import android.util.Log;
+
+/**
+ * @Author moyuwan
+ * @Date 17/8/16
+ */
+public class LibAmap {
+
+    public static void test(String content) {
+        Log.e("LibAmap", content);
+    }
+
+}
